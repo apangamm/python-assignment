4c6ff9bd61178b9f2ac0523599fc78808ea30b88
apangamm <121764591+apangamm@users.noreply.github.com>
2023-04-01 11:46:01 +0530

    Add files via upload

---
+++ python assignment.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+cm_value = float(input("Enter the value in cm: "))
+
+inch_value = cm_value/2.54
+
+print('Value in inch: {} inches'.format(inch_value))
+
+
+# In[1]:
+
+
+inch_value = float(input("Enter the value in inches: "))
+
+cm_value = inch_value*2.54
+
+print('Value in cm: {} cms'.format(cm_value))
+
+
+# In[ ]:
+
+
+
+
